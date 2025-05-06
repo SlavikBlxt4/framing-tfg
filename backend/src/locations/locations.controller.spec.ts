0cdc0ffa89bcb1bf24a9b8f0a3710c16e27e0f8a
SlavikBlxt4 <slavikiftodii5@gmail.com>
2025-05-06 20:28:18 +0200

    locations mocks corregidos

--- backend/src/locations/locations.controller.spec.ts
+++ backend/src/locations/locations.controller.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationsController } from './locations.controller';
+import { LocationsService } from './locations.service';
 
 describe('LocationsController', () => {
   let controller: LocationsController;
+  let service: LocationsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LocationsController],
+      providers: [
+        {
+          provide: LocationsService,
+          useValue: {
+            getAllLocations: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<LocationsController>(LocationsController);
+    service = module.get<LocationsService>(LocationsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return all locations', async () => {
+    const result = await controller.getAllLocations();
+    expect(result).toEqual([]);
+    expect(service.getAllLocations).toHaveBeenCalled();
   });
 });
--- backend/src/locations/locations.service.spec.ts
+++ backend/src/locations/locations.service.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationsService } from './locations.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Location } from './location.entity';
+import { Repository } from 'typeorm';
 
 describe('LocationsService', () => {
   let service: LocationsService;
+  let repo: Repository<Location>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationsService],
+      providers: [
+        LocationsService,
+        {
+          provide: getRepositoryToken(Location),
+          useValue: {
+            find: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<LocationsService>(LocationsService);
+    repo = module.get<Repository<Location>>(getRepositoryToken(Location));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return an empty array', async () => {
+    const result = await service.getAllLocations();
+    expect(result).toEqual([]);
+    expect(repo.find).toHaveBeenCalled();
   });
 });
