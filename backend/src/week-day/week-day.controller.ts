7983d7a35ecc1bfe8cd3dd6f59834e57f9aeabe7
Marcos Crespán <mcrespan@hotmail.es>
2025-05-09 15:57:20 +0200

    Fix

--- backend/src/week-day/week-day.controller.ts
+++ backend/src/week-day/week-day.controller.ts
@@ -1,16 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { WeekDayService } from './week-day.service';
-import { CreateWeekDayDto } from './dto/create-week-day.dto';
-import { UpdateWeekDayDto } from './dto/update-week-day.dto';
+
 
 @Controller('week-day')
 export class WeekDayController {
   constructor(private readonly weekDayService: WeekDayService) {}
-
-  @Post()
-  create(@Body() createWeekDayDto: CreateWeekDayDto) {
-    return this.weekDayService.create(createWeekDayDto);
-  }
 
   @Get()
   findAll() {
@@ -20,11 +14,6 @@ export class WeekDayController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.weekDayService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWeekDayDto: UpdateWeekDayDto) {
-    return this.weekDayService.update(+id, updateWeekDayDto);
   }
 
   @Delete(':id')
