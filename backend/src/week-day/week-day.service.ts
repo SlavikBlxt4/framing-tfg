10732271f25d04e07e8322f77ca9589bdfe7d8da
Marcos Crespán <mcrespan@hotmail.es>
2025-05-09 15:55:25 +0200

    Fix lint

--- backend/src/week-day/week-day.service.ts
+++ backend/src/week-day/week-day.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { CreateWeekDayDto } from './dto/create-week-day.dto';
-import { UpdateWeekDayDto } from './dto/update-week-day.dto';
+
 
 @Injectable()
 export class WeekDayService {
-  create(createWeekDayDto: CreateWeekDayDto) {
-    return 'This action adds a new weekDay';
-  }
 
   findAll() {
     return `This action returns all weekDay`;
@@ -16,9 +12,7 @@ export class WeekDayService {
     return `This action returns a #${id} weekDay`;
   }
 
-  update(id: number, updateWeekDayDto: UpdateWeekDayDto) {
-    return `This action updates a #${id} weekDay`;
-  }
+
 
   remove(id: number) {
     return `This action removes a #${id} weekDay`;
