a5ac6deab79763925dbe89273a6106e1a7d6f340
Marcos Crespán <mcrespan@hotmail.es>
2025-05-09 14:40:50 +0200

    Madre mia, fix lint

--- backend/src/schedule/schedule.controller.ts
+++ backend/src/schedule/schedule.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Delete } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 
 @Controller('schedule')
